13eea28055196b9fbe46a843bce718fd44c6713e
inacio88 <inacio88@outlook.com>
2023-09-24 18:42:08 -0300

    formulário simples de casdastro de produto

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,FlatList } from 'react-native';
 import HomeScreen from './components/HomeScreen';
 import Lista from './components/Lista';
+import FormProduto from './components/FormProduto';
 
 
 export default function App() {
   return (
     <>
-      <Lista/>
+      <FormProduto/>
     </>
   );
 }
